08edfe0614f5b7e8daff27cab20807d2e7946d66
Amanda Fernandes <116132492+AmandaFernandes0701@users.noreply.github.com>
2026-04-19 15:12:54 -0300

    Add: combinations - submission-4

---
+++ Data Structures & Algorithms/combinations/submission-4.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    private List<List<Integer>> resultList = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+
+        List<Integer> currList = new ArrayList<>();
+
+        return dfsAllCombinationsWithLengthK(currList, 1, n, k);       
+    }
+
+    public List<List<Integer>> dfsAllCombinationsWithLengthK(List<Integer> currList, int currNum, int n, int k)
+    {
+        if(currList.size() == k)
+        {
+            resultList.add(new ArrayList<>(currList));
+            return resultList;
+        }
+
+        else if(currNum == n + 1)
+            return resultList;
+
+        // não add currNum
+        dfsAllCombinationsWithLengthK(currList, currNum + 1, n, k);
+
+        // add currNum
+        currList.add(currNum);
+        dfsAllCombinationsWithLengthK(currList, currNum + 1, n, k);
+        currList.remove(currList.size() - 1);
+
+        return resultList;
+    }
+}
