3634436713d42e0d616c85890b4b2d7958103df6
Amanda Fernandes <116132492+AmandaFernandes0701@users.noreply.github.com>
2026-04-18 23:40:50 -0300

    Add: valid-perfect-square - submission-7

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-7.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+
+        long left = 0;
+        long right = num;
+
+        while(left <= right)
+        {
+            long middle = left + (right - left) / 2;
+            long square = middle * middle;
+
+            if(square == num) return true;
+
+            else if(square > num)
+                right = middle - 1;
+            
+            else
+                left = middle + 1;
+        }
+
+        return false;      
+    }
+}
