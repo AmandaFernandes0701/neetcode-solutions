81ee5e03df93efc02d7752a3806a0d8eae30d784
Amanda Fernandes <116132492+AmandaFernandes0701@users.noreply.github.com>
2026-04-24 09:57:56 -0300

    Add: sqrtx - submission-12

---
+++ Data Structures & Algorithms/sqrtx/submission-12.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {                                              
+
+        long left = 0;
+        long right = x;
+
+        while(left <= right)
+        {
+            long middle = left + (right - left) / 2;
+
+            if(middle * middle == x)
+                return (int)middle;
+
+            else if(middle * middle > x)
+                right = middle - 1;
+            
+            else
+                left = middle + 1;
+        }
+
+        return (int)right;
+        
+    }
+}
